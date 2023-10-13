4a9009b77a257623e9d976918dc1c2f5fa844297
Lettie <lettiezulu764@gmail.com>
2023-10-13 12:02:10 +0200

    committed changes

---
+++ chapter14/movie-reviews/frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Switch, Route, Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import AddReview from "./components/add-review"
+import MoviesList from "./components/movies-list"
+import Movie from "./components/movie"
+import Login from "./components/login"
+
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+export default App;
